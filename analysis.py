5b63b54a7d677ca3e5d8cf17bef2830ab56356c3
Envgy <36904996+Envgy@users.noreply.github.com>
2018-03-01 11:26:36 -0800

    Fixing

--- analysis.py
+++ analysis.py
@@ -4,9 +4,10 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
 import pandas as pd
+#working on asia data
 asiadat = pd.read_csv('data/gapminder_gdp_asia.csv', index_col = 'country')
+
 
 
 
